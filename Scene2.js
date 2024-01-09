0496f6a02da71d0e394cdcd59d7244d56d8b7d6a
Kiwi864 <soldi.banka@gmail.com>
2024-01-09 17:38:51 +0100

    f

--- Scene2.js
+++ Scene2.js
@@ -51,7 +51,7 @@ var config = {
             this.ship1 = this.add.sprite(config.width/2 - 50, config.height/2, "ship");
             this.ship2 = this.add.sprite(config.width/2, config.height/2, "ship2");
             this.ship3 = this.add.sprite(config.width/2 + 50, config.height/2, "ship3");
-            this.character = this.add.sprite(config.width/2 + 50, config.height/2, "ship3");
+           // this.character = this.add.sprite(config.width/2 + 50, config.height/2, "ship3");
             this.physics.world.setBounds(0, 20, config.width, config.height-20);
             
 
